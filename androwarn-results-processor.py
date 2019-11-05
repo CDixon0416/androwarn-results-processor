0b5af2bb986b357789b771d99512028ad4e0c3ea
CDixon0416 <caseyericdixon@gmail.com>
2019-11-05 13:27:30 -0500

    Added section to determine upper bound of analysis results

--- androwarn-results-processor.py
+++ androwarn-results-processor.py
@@ -5,15 +5,28 @@ from bs4 import SoupStrainer
 only_body = SoupStrainer('body')
 androwarnHTML = BeautifulSoup(open("com.google.android.apps.docs_1572456480.html"), features="html.parser",
                               parse_only=only_body)
-
 analysisResults = androwarnHTML.find_all('div', attrs={"class": "tab-pane"})
+resultList = androwarnHTML.find_all('ul', attrs={"class": "nav nav-list"})
+liList = re.findall("<li.*", str(resultList))
+iteration = 0
+lowerBound = 0
+upperBound = 0
+for liTag in liList:
+    iteration = iteration + 1
+    if liTag == '<li class="nav-header">Analysis Results</li>':
+        lowerBound = iteration
+    if liTag == '<li class="nav-header">Apk File</li>':
+        upperBound = iteration
+upperIndex = upperBound - lowerBound + 3
+
 resultIndex = 0
-for item in analysisResults:
-    if 3 < resultIndex < len(analysisResults)-17:
-        h2tag = re.findall("<h2>(.*?)<*/*h2>", str(item))
-        h3tag = re.findall("<h3>(.*?)<*/*h3>", str(item))
+for liTag in analysisResults:
+    if 3 < resultIndex < upperIndex:
+        h2tag = re.findall("<h2>(.*?)<*/*h2>", str(liTag))
+        h3tag = re.findall("<h3>(.*?)<*/*h3>", str(liTag))
         print(h2tag)
         print(h3tag)
     resultIndex = resultIndex + 1
 
 # To Do Pull specific values and store into excel document
+# To Do add comments
